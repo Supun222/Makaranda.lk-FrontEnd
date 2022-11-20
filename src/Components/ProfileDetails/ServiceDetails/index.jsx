88f0b36be3becd6efd0440d532a6288a6f3e3530
supun222 <supun222>
2022-11-21 00:28:43 +0530

    added profile page

---
+++ src/Components/ProfileDetails/ServiceDetails/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Locations from "./Locations";
+
+function ServiceDetails() {
+  return (
+    <div className="bg-gray-100 rounded-md w-96 p-5 mt-4">
+      <Locations />
+      <h1>shshs</h1>
+    </div>
+  );
+}
+
+export default ServiceDetails; // some desmon, sometimes men get cold feet 
