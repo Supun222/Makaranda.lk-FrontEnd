8ae5fb269e892a31bf0cf6febd2e1a2177944636
supun222 <supun222>
2023-02-22 22:58:50 +0530

    bug fixed filter panel

--- src/Components/LocationFilter/index.jsx
+++ src/Components/LocationFilter/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "../../axios/index";
@@ -8,8 +9,8 @@ function Location() {
 
   const renderCategories = () => {
     axios
-      .get("/location/all")
-      .then((response) => setsubLocation(response.data.locations));
+      .get("/location/count")
+      .then((response) => setsubLocation(response.data.result));
   };
 
   useEffect(() => {
@@ -29,13 +30,13 @@ function Location() {
           Locations.map((item) => (
             <div
               className="font-Lato text-lg font-semibold text-primary text-start mt-1 capitalize"
-              key={item.location_name}
+              key={item._id}
             >
               <Link
                 to={`/timeline/${category}/${item.location_name}`}
                 className="inline-flex mt-2"
               >
-                {item.location_name} <p className="text-gray-500 ml-2">(122)</p>
+                {item._id} <p className="text-gray-500 ml-2">({item.count})</p>
               </Link>
             </div>
           ))
